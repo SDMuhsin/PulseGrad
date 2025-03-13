751f2c340c5b999757881342f8d563ecba20881f
ckp908 <ckp908@usask.ca>
2025-03-13 08:25:58 -0600

    Duplicate runs

--- sbatch/glue_master.sh
+++ sbatch/glue_master.sh
@@ -3,46 +3,41 @@
 echo "Beginning train_glue.py sbatch script submissions."
 
 
-# Loop through each dataset-model pair
 for dataset in mrpc rte cola stsb qnli qqp mnli sst2; do
-    
-	echo "Submitting job with dataset: $dataset"
-	for dataset in mrpc rte cola stsb qnli qqp mnli sst2; do
-	    case $dataset in
-		mrpc|rte|stsb|cola)
-		    time_req="1-00:00"
-		    ;;
-		sst2)
-		    time_req="4-00:00"
-		    ;;
-		*)
-		    time_req="7-00:00"
-		    ;;
-	    esac
+    case $dataset in
+	mrpc|rte|stsb|cola)
+	    time_req="1-00:00"
+	    ;;
+	sst2)
+	    time_req="4-00:00"
+	    ;;
+	*)
+	    time_req="7-00:00"
+	    ;;
+    esac
 
-	    echo "Submitting job with dataset: $dataset and time: $time_req"
-	    sbatch \
-		--nodes=1 \
-		--ntasks-per-node=1 \
-		--cpus-per-task=1 \
-		--gpus=1 \
-		--mem=16000M \
-		--time=$time_req \
-		--chdir=/scratch/sdmuhsin/DiffGrad2 \
-		--output=${dataset}-%N-%j.out \
-		--wrap="
-		    export TRANSFORMERS_CACHE=\"./data\"
-		    export HF_HOME=\"./data\"
-		    export TORCH_HOME=\"./data\"
-		    module load python/3.10
-		    module load arrow/16.1.0
-		    source ./env/bin/activate
-		    echo 'Environment loaded'
-		    which python3
-		    export PYTHONPATH=\"\$PYTHONPATH:\$(pwd)\"
-		    ./scripts/run_glue.sh $dataset
-		"
-	done
+    echo "Submitting job with dataset: $dataset and time: $time_req"
+    sbatch \
+	--nodes=1 \
+	--ntasks-per-node=1 \
+	--cpus-per-task=1 \
+	--gpus=1 \
+	--mem=16000M \
+	--time=$time_req \
+	--chdir=/scratch/sdmuhsin/DiffGrad2 \
+	--output=${dataset}-%N-%j.out \
+	--wrap="
+	    export TRANSFORMERS_CACHE=\"./data\"
+	    export HF_HOME=\"./data\"
+	    export TORCH_HOME=\"./data\"
+	    module load python/3.10
+	    module load arrow/16.1.0
+	    source ./env/bin/activate
+	    echo 'Environment loaded'
+	    which python3
+	    export PYTHONPATH=\"\$PYTHONPATH:\$(pwd)\"
+	    ./scripts/run_glue.sh $dataset
+	"
 done
 
 echo "All jobs submitted."
