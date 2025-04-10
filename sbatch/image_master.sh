410e601cbc7e122a06bc535488ab7fcfbe1bade2
ckp908 <ckp908@usask.ca>
2025-04-10 09:17:53 -0600

    Vit case

--- sbatch/image_master.sh
+++ sbatch/image_master.sh
@@ -5,9 +5,9 @@ echo "Beginning train_imageclassification.py sbatch script submissions."
 # Define dataset-model pairs based on task difficulty and model size
 declare -A pairs
 #pairs["MNIST"]="squeezenet1_0"
-pairs["FMNIST"]="resnet50"
-pairs["CIFAR10"]="densenet121"
-#pairs["CIFAR100"]="vit_b_16"
+#pairs["FMNIST"]="resnet50"
+#pairs["CIFAR10"]="densenet121"
+pairs["CIFAR100"]="vit_b_16"
 #pairs["STL10"]="efficientnet_v2_s"
 
 # List of optimizers to loop through
@@ -29,7 +29,7 @@ for dataset in "${!pairs[@]}"; do
             --cpus-per-task=1 \
             --gpus=1 \
             --mem=8000M \
-            --time=4-00:00 \
+            --time=7-00:00 \
             --chdir=/scratch/sdmuhsin/DiffGrad2 \
             --output=${optimizer}-${model_filename}-${dataset}-%N-%j.out \
             --wrap="
@@ -42,7 +42,7 @@ for dataset in "${!pairs[@]}"; do
                 echo 'Environment loaded'
                 which python3
                 export PYTHONPATH=\"\$PYTHONPATH:\$(pwd)\"
-                python3 src/train_imageclassification.py --dataset=$dataset --model=$model --optimizer=$optimizer --epochs=100 --batch_size=64 --lr=0.001
+                python3 src/train_imageclassification.py --dataset=$dataset --model=$model --optimizer=$optimizer --epochs=100 --batch_size=64 --lr=0.00005
             "
     done
 done
